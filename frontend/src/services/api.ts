f1737b8ac90999c5eae654b27bab78fd49329476
KaiWhen <wangkw523@gmail.com>
2025-06-27 13:03:42 +0100

    Add base url in vite config

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,7 +5,7 @@ const API_URL: string = import.meta.env.API_BASE_URL ?? "http://localhost:3000";
 export async function fetchNasaData<T>(url: string): Promise<ApiResponse<T>> {
   const response = await fetch(`${API_URL}${url}`);
   if (!response.ok) {
-    throw new Error(`Error fetching APOD: ${response.statusText}`);
+    throw new Error(`Error fetching data: ${response.statusText}`);
   }
   const data: ApiResponse<T> = await response.json();
   return data;
