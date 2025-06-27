529d9c685957c2b99f5681460c2b3147472ebf54
KaiWhen <wangkw523@gmail.com>
2025-06-27 02:19:22 +0100

    add api folder for express routes

---
+++ backend/api/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import { Request } from "express-serve-static-core";
+import apodRouter from "../src/routes/apod.route.js";
+import roverRouter from "../src/routes/rover.route.js";
+import insightRouter from "../src/routes/insight.route.js";
+
+const __dirname = path.resolve();
+const app = express();
+const port = process.env.PORT ?? "9001";
+
+app.use(cors<Request>());
+
+// if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+// }
+
+app.use("/apod", apodRouter);
+app.use("/rover", roverRouter);
+app.use("/insight", insightRouter);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
